c02f8c385a0a09719f6144bdce49e1c77e6859c2
anuarmusa <anuarmusa@tmrnd.com.my>
2024-10-07 12:44:08 +0800

    Updated

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 
+
 # interface
 import streamlit as st
 
