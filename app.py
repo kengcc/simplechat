f23e2870b8362cfe88c8d3d6fd05f49b8283848e
Anuar Musa <anuarmusa@tmrnd.com.my>
2024-10-07 00:51:37 +0000

    Create a simple chat app using OpenAI API

--- app.py
+++ app.py
@@ -1,1 +1,38 @@
 
+#langchain dependencies
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+# interface
+import streamlit as st
+
+#environment variables
+import os 
+from dotenv import load_dotenv
+load_dotenv()
+
+os.environ["OPENAI_API_KEY"]=os.getenv("OPENAI_API_KEY")
+## Langmith tracking
+os.environ["LANGCHAIN_TRACING_V2"]="true"
+os.environ["LANGCHAIN_API_KEY"]=os.getenv("LANGCHAIN_API_KEY")
+
+#prompt template
+prompt=ChatPromptTemplate.from_messages(
+    [
+        ("system", "You are a helpful assistance. Please respond to user queries."),
+        ("user", "Question:{question}")
+    ]
+)
+
+## Streamlit Framework
+st.title('Langchain Demo with OpenAI')
+input_text=st.text_input("Search the topic you want.")
+
+#OpenAI LLM
+llm=ChatOpenAI(model="gpt-3.5-turbo")
+output_parser = StrOutputParser()
+chain=prompt|llm|output_parser
+
+if input_text:
+    st.write(chain.invoke({'question':input_text}))
