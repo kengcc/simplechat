d8c30ff57edd1ee17bda342750edf9fa3b962591
anuarmusa <anuarmusa@tmrnd.com.my>
2024-10-25 08:32:32 +0800

    "Add license term"

--- app.py
+++ app.py
@@ -1,5 +1,25 @@
+# Copyright (c) 2024 Telekom Research and Development Sdn BHd
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+# DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+# OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
+# OR OTHER DEALINGS IN THE SOFTWARE.
+#
 
-#langchain dependencies
+# langchain dependencies
 from langchain_openai import ChatOpenAI    # LLM Model
 from langchain_core.prompts import ChatPromptTemplate # Prompt Input
 from langchain_core.output_parsers import StrOutputParser # Output Display
@@ -30,7 +50,7 @@ prompt=ChatPromptTemplate.from_messages(
 st.title('Langchain Demo with OpenAI')
 input_text=st.text_input("Search the topic you want.")
 
-#OpenAI LLM
+# OpenAI LLM
 llm=ChatOpenAI(model="gpt-3.5-turbo")
 output_parser = StrOutputParser()
 chain=prompt|llm|output_parser
